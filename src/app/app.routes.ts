d5bcc7a3d1e4f9535c33130daad2c599c1cd0005
ZeinebouEthmaneDabo <zeineboudabo2003@gmail.com>
2025-03-07 13:45:00 +0000

    improve auth guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,16 +3,16 @@ import { LoginComponent } from './components/login/login.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ProduitsComponent } from './components/produits/produits.component';
 import { CommandesComponent } from './components/commandes/commandes.component';
-import { authGuard } from './auth.guard';
+import { AuthGuard } from './auth.guard';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   {
     path: 'dashboard',
     component: DashboardComponent,
-    canActivate: [authGuard],
+    canActivate: [AuthGuard],
   },
-  { path: 'products', component: ProduitsComponent, canActivate: [authGuard] },
-  { path: 'commands', component: CommandesComponent, canActivate: [authGuard] },
+  { path: 'products', component: ProduitsComponent, canActivate: [AuthGuard] },
+  { path: 'commands', component: CommandesComponent, canActivate: [AuthGuard] },
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
 ];
--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -4,16 +4,45 @@ import { CanActivate, Router } from '@angular/router';
 @Injectable({
   providedIn: 'root',
 })
-export class authGuard implements CanActivate {
+export class AuthGuard implements CanActivate {
   constructor(private router: Router) {}
 
   canActivate(): boolean {
     const token = localStorage.getItem('token');
+
     if (token) {
-      return true;
+      try {
+        const decodedToken = this.decodeToken(token);
+        if (decodedToken.role === 'ADMIN') {
+          return true;
+        } else {
+          this.router.navigate(['/unauthorized']); // Redirect non-admins
+          return false;
+        }
+      } catch (error) {
+        console.error('Invalid token', error);
+        this.router.navigate(['/login']);
+        return false;
+      }
     } else {
       this.router.navigate(['/login']);
       return false;
+    }
+  }
+
+  private decodeToken(token: string): any {
+    try {
+      const base64Url = token.split('.')[1]; // Extract payload
+      const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+      const jsonPayload = decodeURIComponent(
+        atob(base64)
+          .split('')
+          .map((c) => '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2))
+          .join('')
+      );
+      return JSON.parse(jsonPayload);
+    } catch (error) {
+      throw new Error('Invalid token format');
     }
   }
 }
